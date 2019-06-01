aa50b4016cf2eddd8a86854f73fb1a3f4e56fd3c
Anna VM <annamitchell@annavm.us-west1-b.c.cs224u-242203.internal>
2019-06-01 02:51:52 +0000

    make constants more intelligible

--- constants.py
+++ constants.py
@@ -3,12 +3,11 @@ sys.path.append('..')
 # import env
 
 # Global constants.
-DATA_FOLDER = 'data/'
 
-DATA_PATH = 'data'
+DATA_PATH = 'data/'
 
-RAW_PATH = DATA_PATH + '/CLOTH'
-CLEANED_PATH = DATA_PATH + '/cleaned'
+RAW_PATH = DATA_PATH + 'CLOTH/'
+CLEANED_PATH = DATA_PATH + 'cleaned/'
 
-DATA_TYPES = ['/high', '/middle']
-DATA_SPLITS = ['/train', '/test', '/valid']
+DATA_TYPES = ['high/', 'middle/']
+DATA_SPLITS = ['train/', 'test/', 'valid/']
