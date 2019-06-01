bc542ab5bd4403e2da774acfaf3a398d85d60592
Anna VM <annamitchell@annavm.us-west1-b.c.cs224u-242203.internal>
2019-06-01 02:46:11 +0000

    fixing constants

--- constants.py
+++ constants.py
@@ -6,11 +6,9 @@ sys.path.append('..')
 DATA_FOLDER = 'data/'
 
 DATA_PATH = 'data'
-SOURCE_PATH = DATA_PATH + '/CLOTH'
-DATA_TYPES = ['/high', '/middle']
 
+RAW_PATH = DATA_PATH + '/CLOTH'
+CLEANED_PATH = DATA_PATH + '/cleaned'
+
+DATA_TYPES = ['/high', '/middle']
 DATA_SPLITS = ['/train', '/test', '/valid']
-DEST_PATH = DATA_PATH + '/cleaned'
-TRAIN_DEST_PATH = DEST_PATH + '/train'
-TEST_DEST_PATH = DEST_PATH + '/test'
-VALID_DEST_PATH = DEST_PATH + '/valid'
